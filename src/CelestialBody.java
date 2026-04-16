30979b789b66cfeb89c896e23dc7c85fff03d572
KarthikAmaram <amaramkart09@gmail.com>
2026-04-15 21:20:12 -0500

    Added new class and created constructor

---
+++ src/CelestialBody.java
@@ -0,0 +1,12 @@
+public class CelestialBody {
+    public double x, y, vx, vy, mass, radius;
+
+    public CelestialBody(double x, double y, double vx, double vy, double mass, double radius) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.mass = mass;
+        this.radius = radius;
+    }
+}
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+
 public class Simulation {
+    private static final double G = Math.pow(6.67, -11);
+    private ArrayList<CelestialBody> celestialBodies;
+
+    public Simulation() {
+        celestialBodies = new ArrayList<>();
+    }
+
+    public ArrayList<CelestialBody> getCelestialBodies() {
+        return celestialBodies;
+    }
     public void start() {
         System.out.println("Yo first line");
     }
